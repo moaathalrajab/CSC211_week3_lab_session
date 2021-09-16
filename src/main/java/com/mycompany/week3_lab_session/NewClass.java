4526fb49043fb5b9df82d231ea683f5d166a2b84
itlabs <itlabs@WHB019_1.it.farmingdale.edu>
2021-09-16 14:57:43 -0400

    Add skeleton code of getArrayFromFile

--- src/main/java/com/mycompany/week3_lab_session/NewClass.java
+++ src/main/java/com/mycompany/week3_lab_session/NewClass.java
@@ -1,9 +1,15 @@
 
 package com.mycompany.week3_lab_session;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class NewClass {
     
     
     boolean isPrime(int n){
+       
         for(int i=2; i<n; i++){
             if(n%i == 0) 
             {
@@ -23,12 +29,21 @@ public class NewClass {
         return sm;
     }
     
+    int[] getArrayFromFile(String fl) throws FileNotFoundException{
+        Scanner sc=new Scanner(new File(fl));
+        while(sc.hasNext()){
+            
+        }
+        int[] arr=new int[10];
+        
+        return arr;
+    }
+    
     public static void main(String[] args) {
         NewClass nc= new NewClass();
         int[] arr={3,4,8,7,9,10,12,5,11};
+        
         int sum= nc.getPrimeSum(arr);
-        System.out.println("The sum of the primes in the array is "+ sum );
-        
-        
+        System.out.println("The sum of the primes in the array is "+ sum ); 
     }
 }
