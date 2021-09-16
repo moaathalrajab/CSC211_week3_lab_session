fbe3533a8ef21d43a4dce7446dfa4aaba0bcac1c
itlabs <itlabs@WHB019_1.it.farmingdale.edu>
2021-09-16 14:37:53 -0400

    Add getPrimeSum method to calculate the sum of all primes in an array

--- src/main/java/com/mycompany/week3_lab_session/NewClass.java
+++ src/main/java/com/mycompany/week3_lab_session/NewClass.java
@@ -1,6 +1,8 @@
 
 package com.mycompany.week3_lab_session;
 public class NewClass {
+    
+    
     boolean isPrime(int n){
         for(int i=2; i<n; i++){
             if(n%i == 0) 
@@ -11,11 +13,22 @@ public class NewClass {
         return true;
     }
 
+    int getPrimeSum(int[] nums){
+        int sm=0;
+        for(int i=0; i<nums.length; i++){
+            if(isPrime(nums[i])){
+                sm+=nums[i];
+            }
+        }
+        return sm;
+    }
+    
     public static void main(String[] args) {
         NewClass nc= new NewClass();
-        int x= 7;
-        System.out.println("Number "+x+" is " +(
-                nc.isPrime(x)?"":" not a "
-                )+ " prime "   );
+        int[] arr={3,4,8,7,9,10,12,5,11};
+        int sum= nc.getPrimeSum(arr);
+        System.out.println("The sum of the primes in the array is "+ sum );
+        
+        
     }
 }
